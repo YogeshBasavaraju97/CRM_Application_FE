d3e9792108b49f96f08411a43cfcda493b29376b
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2025-04-07 16:52:09 +0530

    url updated

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,4 @@
 // export const BASE_URL =
 //   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
-
-export const BASE_URL = "/api";
-
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
