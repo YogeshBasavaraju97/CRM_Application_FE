68d651c709dd2c5b8b10d3e113a860438e48881c
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2025-04-07 15:04:20 +0530

    proxy server added

--- utils/constants.js
+++ utils/constants.js
@@ -3,4 +3,6 @@
 // export const BASE_URL =
 //   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const BASE_URL = "/api";
+
